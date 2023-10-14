45d1f000b0614ab03a69dcf9d2e4fd7235f15acc
Shaik Ashraf ali <33453644+aliashraf90@users.noreply.github.com>
2023-10-14 10:26:32 +0530

    how to clear a list

---
+++ clear_a_list.py
@@ -0,0 +1,11 @@
+#approch1 using clear() method
+mylist = [6,0,4,1]
+mylist.clear() # here we use variable.clear() method
+print(mylist)
+
+######approch 2
+###intializing the list with no values
+mylist = [6,0,4,1]
+mylist = []
+print(mylist)
+
