17a0797fed8f041b1b664f51ac9d6417c697524d
Shaik Ashraf ali <33453644+aliashraf90@users.noreply.github.com>
2023-10-14 10:33:17 +0530

    program for reversing the list

---
+++ reverse_list.py
@@ -0,0 +1,9 @@
+# stright forword method with the use of reverse() method
+my_list = [10,12,14,15,17]
+print(my_list,"my list before reverse")
+
+my_list.reverse()
+print(my_list,'after reverse')
+# using slicing method
+my_list = [10,12,14,15,17]
+print(my_list[::-1])
